f129a6ff9c134093d197147231dd123060fd0142
Keerthanaaa-24 <keerthanamr1307@gmail.com>
2026-03-19 20:53:56 +0530

    Search Element and Remove without function

--- basics/arrays/search.py
+++ basics/arrays/search.py
@@ -1,9 +1,13 @@
 arr = list(map(int,input("Enter elements:").split()))
 
 key = int(input("Enter element to remove the founded element: "))
-if key in arr:
-        arr.remove(key)
-        print("Element found and removed")
-else:
-        print("Element not found")
-print(" Updated Array:",arr) 
+new_arr = []
+removed= False
+for x in arr:
+        if x == key and not removed:
+                removed = True
+                continue
+        else:
+                new_arr.append(x)
+print(new_arr)     
+ 
