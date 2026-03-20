190bb4c90d0732eec54a32acd2d6065175e064e2
Keerthanaaa-24 <keerthanamr1307@gmail.com>
2026-03-20 22:59:14 +0530

    Frequency count

---
+++ basics/arrays/testprograms/freqcount.py
@@ -0,0 +1,10 @@
+nums = [1, 2, 2, 3, 3, 3,4,4,4,6,7,5,6,7,6,7,8,9,2,1,3]
+freq = {}
+
+for n in nums:
+    if n in freq:
+        freq[n] = freq[n] + 1
+    else:
+        freq[n] = 1
+
+print(freq)
