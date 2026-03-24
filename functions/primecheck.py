e1367f67662a06e5c53bbfe13967dcc6885184d8
Keerthanaaa-24 <keerthanamr1307@gmail.com>
2026-03-24 21:35:39 +0530

    Prime Check using Function

---
+++ functions/primecheck.py
@@ -0,0 +1,14 @@
+def prime(n):
+    if n <= 1:
+        return False
+
+    for i in range(2, n // 2):
+        if n % i == 0:
+            return False
+    return True
+num = int(input("Enter a number: "))
+
+if prime(num):
+    print(num, "is a Prime Number.")
+else:
+    print(num, "is not a Prime Number.")
