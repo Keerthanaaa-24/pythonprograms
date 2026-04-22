009b1a5385cd49961cce75b9e78fb1f692c9483b
Keerthu <keerthanamr1307@gmail.com>
2026-04-22 12:17:56 +0530

    whileloop.py

--- basics/whileloop.py
+++ basics/whileloop.py
@@ -1,13 +1,9 @@
 name =input("Enter your name: ")
 age=int(input("Enter your age: "))
-
-
 while name=="":
     name=input("Enter your name: ")
-
 while age < 0:
     print(f"Age cannot be 0")
     age=int(input("Enter your age: "))
-
 print(f"Hey {name}")
 print(f"You're {age} years old!")
